5c7099050b2892490d1736b1190ceccc4d91b196
Erinaldo Prado <60299109+eri-prado@users.noreply.github.com>
2021-12-16 18:36:45 -0300

    style changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // 'use strict'
 
-// // Menu mobile
-// const btnMobile = document.querySelector('#btn-mobile');
+// Menu mobile
+const btnMobile = document.querySelector('.btn-mobile');
 
-// function toggleMenu(event) {
-//   if (event.type === 'touchstart') event.preventDefault();
-//   const nav = document.querySelector('.menu-nav');
-//   nav.classList.toggle('active');
-//   const active = nav.classList.contains('active');
-//   event.currentTarget.setAttribute('aria-expanded', active);
-//   if (active) {
-//     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-//   } else {
-//     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-//   }
-// }
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.querySelector('.menu-nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
 
-// btnMobile.addEventListener('click', toggleMenu);
-// btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
 
 // // Text typing animation
 // const title = document.querySelector('.call-to-action > h1')
