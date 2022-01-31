ceb165383dc82d2af4dc36ec18c6a3a2dd2956d9
Erinaldo Prado <eri.prado@hotmail.com>
2022-01-31 19:17:40 -0300

    style changes

--- js/script.js
+++ js/script.js
@@ -2,18 +2,6 @@
 
 // TEXT TYPING ANIMATION
 
-// const title = document.querySelector('.cta h1')
-
-// function typeWriter(element) {
-//   const textArray = element.innerHTML.split('')
-//   element.innerHTML = ''
-//   textArray.forEach((letter, index) => {
-//     setTimeout(() => element.innerHTML += letter, 75 * index)
-//   })
-// }
-// typeWriter(title)
-
-
 var typed = new Typed('.type', {
   strings: ['website', 'ecommerce', 'design'],
   typeSpeed: 120,
