9b5a14e93696873049f20dd8c6fe208e90b0003a
Erinaldo Prado <eri.prado@hotmail.com>
2022-02-01 13:51:25 -0300

    style changes

--- js/script.js
+++ js/script.js
@@ -2,9 +2,48 @@
 
 // TEXT TYPING ANIMATION
 
-var typed = new Typed('.type', {
+const typed = new Typed('.type', {
   strings: ['website', 'ecommerce', 'design'],
   typeSpeed: 120,
   backSpeed: 70,
   loop: true,
 });
+
+// ANIMATE
+
+const debounce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  target.forEach(function (element) {
+    if ((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if (target.length) {
+  window.addEventListener('scroll', debounce(function () {
+    animeScroll();
+  }, 200));
+}
